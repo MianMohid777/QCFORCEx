2def94f5a6397d3a39cf113f02acbf80bb2253a5
Mohid <mohid.jillani@gmail.com>
2023-12-11 02:21:44 +0500

    Add Category E2E Test Case Cypress.io

---
+++ E-commerce Project/Spring_Project/src/test/Cypress/addCategory.cy.js
@@ -0,0 +1,22 @@
+describe('Category Test', () => {
+  it('verifies success alert', () => {
+    cy.visit('http://localhost:8080/');
+    cy.get('.btn-secondary').click();
+    cy.get('#username').type('admin');
+    cy.get('#password').type('aaa');
+    cy.get('.btn').click();
+    cy.get(':nth-child(1) > .card > .card-body > .card-link').click();
+    cy.get('[style="margin: 20px 0"]').click();
+
+    
+    cy.on('window:alert', (alertMessage) => {
+
+      expect(alertMessage).to.contain('Category Added Successfully'); 
+    });
+
+    const currentTime = new Date().toLocaleTimeString();
+    const newText =  'Test ' + currentTime;
+    cy.get('#name').wait(1000).type(newText); 
+    cy.get('form > .modal-footer > .btn-primary').click();
+  });
+});
